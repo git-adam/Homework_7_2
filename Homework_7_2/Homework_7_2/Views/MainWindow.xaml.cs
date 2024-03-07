5a12dfddbcd0f9c01c6476f6c16e5ec4d9c63548
git-adam <awwojcik1@gmail.com>
2024-03-07 21:35:23 +0100

    MVVM

--- Homework_7_2/Homework_7_2/Views/MainWindow.xaml.cs
+++ Homework_7_2/Homework_7_2/Views/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Homework_7_2
+namespace Homework_7_2.Views
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
